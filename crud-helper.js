592b3a8813decaa1205f208c8fa83edb2eba24fa
lesterino <lesterleongjr@gmail.com>
2021-07-21 13:55:25 -0700

    first commit

---
+++ crud-helper.js
@@ -0,0 +1,10 @@
+// Connect to the database
+require('dotenv').config();
+require('./config/database');
+
+// Require the Mongoose models
+// const User = require('./models/user');
+// const Puppy = require('./models/puppy');
+
+// Local variables will come in handy
+// let u, p;
---
+++ src/pages/App/App.jsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <main className="App">
+      Puppies Lab
+    </main>
+  );
+}
+
+export default App;
